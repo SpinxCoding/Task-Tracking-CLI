62254cd4e5d06cbf531acb9f4f0880659146d7d0
SpinxCoding <spinxfn@gmail.com>
2025-07-08 19:33:57 -0400

    t

--- task_tracker_cli.py
+++ task_tracker_cli.py
@@ -1,4 +1,14 @@
 import sys
 import json
 
-#Have [add, update, delte tasks]
+#Have [add, update, delete] tasks
+
+task_types = ["add", "update", "delete"]
+
+def add_task(arg):
+    if arg:
+        print("Task added successfully")
+        return
+
+if sys.argv[2] == task_types[0]:
+    add_task(sys.argv[3])
