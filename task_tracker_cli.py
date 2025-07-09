696229b2c00493cd91124476bd82921213faaa4c
SpinxCoding <spinxfn@gmail.com>
2025-07-09 02:27:06 -0400

    Added better functionality and Storage

--- task_tracker_cli.py
+++ task_tracker_cli.py
@@ -3,10 +3,15 @@ import json
 
 #Have [update, delete] tasks
 
+with open("task_storage.json") as file:
+    try:
+        task_storage = json.load(file)
+    except json.decoder.JSONDecodeError:
+        task_storage = []
 
 
 with open("task_storage.json") as file:
-    id = len(list(file)) + 1
+    id = len(task_storage) + 1
 
 
 
@@ -21,7 +26,16 @@ if sys.argv[1] != "task-cli":
 
 def add_task(arg):
     if arg:
-        print("Task added successfully")
+        with open("task_storage.json", "w") as file:
+            global task_storage
+            try:
+                task_storage.append({"task": arg, "id": id})
+                json.dump(task_storage, file,indent=2)
+            except IndexError:
+                task_storage.append({"task": arg, "id": id})
+                json.dump(task_storage, file)
+            file.write("\n")
+        print(f"Task added successfully ({id})")
         return
 
 if sys.argv[2] == task_types[0]:
